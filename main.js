84a8ae4019327309edca9b2f4b10340b3a286c1f
Bernard Nikolić <90216139+nikolicb93@users.noreply.github.com>
2025-06-11 15:44:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const t = document.getElementById("test");
 //const arr = [1,2,3,4]
 window.localStorage.setItem("1", "jedan");
-const storage2 = JSON.stringify(window.localStorage)
+let storage2 = JSON.stringify(window.localStorage)
 t.innerHTML = storage2
 
 
