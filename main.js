63e29d856a160faac24241bbe5fdb575c2cbf211
Bernard Nikolić <90216139+nikolicb93@users.noreply.github.com>
2025-06-11 15:49:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-const t = document.getElementById("test");
-//const arr = [1,2,3,4]
-window.localStorage.setItem("1", "jedan");
-let storage2 = localStorage.getItem("1")
-t.innerHTML = storage2
-
-
 
